827fc1330920eeb5b1a7a8cba3a4ba601d056fda
adi-H <adi.haimsohn@gmail.com>
2022-01-22 19:45:58 +0200

    added nats-wrapper mockup for tests

--- brands/src/test/setup.ts
+++ brands/src/test/setup.ts
@@ -14,6 +14,8 @@ import { app } from '../app';
 // but adding a force exit worked and i dont wanna keep working on this
 // so to be fixed sometime in the future i dunno
 
+jest.mock('../nats-wrapper');
+
 let mongo: any;
 
 beforeAll(async () => {
