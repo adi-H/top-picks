cd8852a97530c4c088703370e1342f8d7420c968
adi-H <adi.haimsohn@gmail.com>
2022-01-22 13:27:54 +0200

    added test infra

---
+++ product/src/routes/tests/ping.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+// this is the health check test
+// cause why not lol
+it('responds pong', async () => {
+	const res = await request(app).get('/api/product/ping').send().expect(200);
+	// console.log(res.text);
+
+	expect(res.text).toEqual('pong');
+});
