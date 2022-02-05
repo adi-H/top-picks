b796c2f00ab26ee752736a21c360cf8ab7727239
adi-H <adi.haimsohn@gmail.com>
2022-02-05 22:49:01 +0200

    fixed existing testing lmao

--- products/src/routes/tests/index.test.ts
+++ products/src/routes/tests/index.test.ts
@@ -6,23 +6,30 @@ import mongoose from 'mongoose';
 const createBrand = async () => {
 	const brand = Brand.build({
 		name: 'blah',
-		id: new mongoose.Types.ObjectId().toHexString()
+		id: new mongoose.Types.ObjectId().toString()
 	});
 	await brand.save();
 	return brand;
 };
 
+const testImgPath = './../../__mocks__/alien.png';
+const createProduct = async (name: string, type: string) => {
+	const brand = await createBrand();
+	const res = await request(app)
+		.post('/api/products')
+		.field('name', 'test')
+		.field('productType', 'blahblah')
+		.field('brand', brand.id)
+		.attach('productImg', __dirname + testImgPath)
+		.expect(201);
+
+	return res;
+};
+
 it('returns 200 with a valid product in db', async () => {
 	const brand = await createBrand();
 
-	await request(app)
-		.post('/api/products')
-		.send({
-			name: 'test',
-			productType: 'adkjsd',
-			brand: brand.id
-		})
-		.expect(201);
+	await createProduct('test', 'testtype');
 
 	const res = await request(app).get('/api/products').expect(200);
 	expect(res.body).toHaveLength(1);
