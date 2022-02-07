8326e026d86e09dfe750ee8ec3b1b1b1d6630e72
adi-H <adi.haimsohn@gmail.com>
2022-02-07 15:36:44 +0200

    added correct url

--- client/src/components/home-page/product-card.js
+++ client/src/components/home-page/product-card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Spacer, Stack, Button, Badge, Image, Text, Flex } from '@chakra-ui/react';
 import { SERVER_URL } from '../../variables/server-url';
+
 // DOCUMENTATION basic card component
 // https://www.geeksforgeeks.org/reactjs-chakra-ui-card-component/
 export const ProductCard = (props) => {
@@ -10,14 +11,14 @@ export const ProductCard = (props) => {
 		<Box w="200px" rounded="20px" overflow="hidden" bg={'gray.200'}>
 			<Image
 				// src="https://media.geeksforgeeks.org/wp-content/uploads/20210727094649/img1.jpg"
-				src={`${SERVER_URL}/api/products/img/${props.productImg}`}
+				src={`${SERVER_URL}${props.imgPath}`}
 				alt="product Image"
 				// boxSize="300px"
 			/>
 			<Box p={5}>
 				<Stack align="center">
 					<Badge variant="solid" colorScheme="green" rounded="full" px={2}>
-						{props.brand}
+						{props.brand.name}
 					</Badge>
 				</Stack>
 				<Stack align="center">
