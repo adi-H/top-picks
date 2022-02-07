83b4a4952b2c0aa754c6ad53f970f23b3b617909
adi-H <adi.haimsohn@gmail.com>
2022-02-07 15:59:53 +0200

    finished basic card

--- client/src/components/home-page/product-card.js
+++ client/src/components/home-page/product-card.js
@@ -4,17 +4,12 @@ import { SERVER_URL } from '../../variables/server-url';
 
 // DOCUMENTATION basic card component
 // https://www.geeksforgeeks.org/reactjs-chakra-ui-card-component/
+
 export const ProductCard = (props) => {
 	console.log(props);
-	// return <div> {props.name} card </div>;
 	return (
 		<Box w="200px" rounded="20px" overflow="hidden" bg={'gray.200'}>
-			<Image
-				// src="https://media.geeksforgeeks.org/wp-content/uploads/20210727094649/img1.jpg"
-				src={`${SERVER_URL}${props.imgPath}`}
-				alt="product Image"
-				// boxSize="300px"
-			/>
+			<Image src={`${SERVER_URL}${props.imgPath}`} alt="product Image" />
 			<Box p={5}>
 				<Stack align="center">
 					<Badge variant="solid" colorScheme="green" rounded="full" px={2}>
@@ -29,12 +24,8 @@ export const ProductCard = (props) => {
 				</Stack>
 				<Flex>
 					<Spacer />
-					{/* <Button variant="solid" colorScheme="green" size="sm">
-						Learn More
-					</Button> */}
 				</Flex>
 			</Box>
-			{/* {props.name} */}
 		</Box>
 	);
 };
