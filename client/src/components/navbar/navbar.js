57a860ba57836311fcb8711e60dda76df66ac199
adi-H <adi.haimsohn@gmail.com>
2022-02-07 23:44:05 +0200

    moved color mode button in navbar comps

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -4,23 +4,18 @@ import { NavBarLogo as Logo } from './navbar-logo';
 import { NavBarContainer } from './navbar-container';
 import { MenuStack } from './navbar-menu-stack';
 import './../../assets/stylesheets/navbar.css';
-import { useColorMode } from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { Button } from '@chakra-ui/react';
 
 // DOCUMENTATION
 // https://raptis.wtf/blog/create-a-navbar-with-chakra-ui-react/
 
 export const NavBar = (props) => {
 	const [ isOpen, setIsOpen ] = React.useState(false);
-	const { colorMode, toggleColorMode } = useColorMode();
 
 	const toggle = () => setIsOpen(!isOpen);
 
 	return (
 		<NavBarContainer {...props}>
 			<Logo w="100px" />
-			<Button onClick={toggleColorMode}>{colorMode === 'light' ? <MoonIcon /> : <SunIcon />}</Button>
 			<MenuToggle toggle={toggle} isOpen={isOpen} />
 			<MenuStack isOpen={isOpen} />
 		</NavBarContainer>
