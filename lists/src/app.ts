509bff3bbc69be7e730daab7b2186bc1d1605c12
adi-H <adi.haimsohn@gmail.com>
2022-02-18 18:48:33 +0200

    added middleware + error infra

--- lists/src/app.ts
+++ lists/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 // import 'express-async-errors';
 
 import { pingRouter } from './routes/ping';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,9 +15,9 @@ app.use(pingRouter);
 
 app.all('*', async (req, res) => {
 	console.log(req);
-	// throw new NotFoundError();
+	throw new NotFoundError();
 });
 
-// app.use(errorHandler);
+app.use(errorHandler);
 
 export { app };
