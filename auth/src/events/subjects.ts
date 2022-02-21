a3f3e466b066214288b8516e594e40d160253e90
adi-H <adi.haimsohn@gmail.com>
2022-02-21 12:43:44 +0200

    added a list created listener

--- auth/src/events/subjects.ts
+++ auth/src/events/subjects.ts
@@ -2,5 +2,10 @@ export enum Subjects {
 	userCreated = 'user:created',
 	// userUpdated = 'user:updated',
 
-	newRatingPosted = 'user-rating:posted'
+	newRatingPosted = 'user-rating:posted',
+
+	newListCreated = 'user-list:created',
+	listDeleted = 'user-list:deleted',
+	listDetailsUpdated = 'user-list:details-updated',
+	listProductCountUpdated = 'user-list:products-updated'
 }
