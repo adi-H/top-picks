83f587be8b95693d1ea6aa37352f2724dc8360bb
adi-H <adi.haimsohn@gmail.com>
2022-03-11 19:41:17 +0200

    added basic product page with server loading

--- client/src/components/home-page/product-card.js
+++ client/src/components/home-page/product-card.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Spacer, Stack, Badge, Image, Text, Flex, useColorModeValue } from '@chakra-ui/react';
+import { Link, Box, Spacer, Stack, Badge, Image, Text, Flex, useColorModeValue } from '@chakra-ui/react';
 import { SERVER_URL } from '../../variables/server-url';
+import { Link as RouterLink } from 'react-router-dom';
 
 // DOCUMENTATION basic card component
 // https://www.geeksforgeeks.org/reactjs-chakra-ui-card-component/
@@ -11,23 +12,25 @@ export const ProductCard = (props) => {
 	console.log(props);
 	return (
 		<Box w="200px" m={7} rounded="20px" overflow="hidden" bg={cardBg}>
-			<Image src={`${SERVER_URL}${props.imgPath}`} alt="product Image" />
-			<Box p={5}>
-				<Stack align="center">
-					<Badge variant="solid" colorScheme="green" rounded="full" px={2}>
-						{props.brand.name}
-					</Badge>
-				</Stack>
-				<Stack align="center">
-					<Text as="h2" fontWeight="normal" my={2}>
-						{props.name}
-					</Text>
-					<Text fontWeight="light">{props.description}</Text>
-				</Stack>
-				<Flex>
-					<Spacer />
-				</Flex>
-			</Box>
+			<Link as={RouterLink} to={`/product/id/${props.id}`}>
+				<Image src={`${SERVER_URL}${props.imgPath}`} alt="product Image" />
+				<Box p={5}>
+					<Stack align="center">
+						<Badge variant="solid" colorScheme="green" rounded="full" px={2}>
+							{props.brand.name}
+						</Badge>
+					</Stack>
+					<Stack align="center">
+						<Text as="h2" fontWeight="normal" my={2}>
+							{props.name}
+						</Text>
+						<Text fontWeight="light">{props.description}</Text>
+					</Stack>
+					<Flex>
+						<Spacer />
+					</Flex>
+				</Box>
+			</Link>
 		</Box>
 	);
 };
--- client/src/views/product-page.js
+++ client/src/views/product-page.js
@@ -1,36 +1,50 @@
-import { Heading } from '@chakra-ui/react';
+import { Box, GridItem, Heading, SimpleGrid, Image, Skeleton, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import { ProductsComp } from '../components/home-page/products-comp';
-// import { getAllProducts } from './../services/products';
+import { useFetch } from '../hooks/use-fetch';
+import { getOneProductConf } from '../services/products';
+import { SERVER_URL } from '../variables/server-url';
 
 export const ProductPage = (props) => {
 	let { productId } = useParams();
 
-	// let [ products, setProducts ] = useState([]);
-	// useEffect(() => {
-	// 	getAllProducts()
-	// 		.then((res) => {
-	// 			setProducts([ ...res.data ]);
-	// 		})
-	// 		.then(() => {
-	// 		});
-	// }, []);
+	let [ isLoading, setIsLoading ] = useState(true);
+	let [ product, setProduct ] = useState({});
+	let conf = getOneProductConf(productId);
+	let { status, data } = useFetch(conf);
+
+	useEffect(
+		() => {
+			setProduct(data);
+			// console.log(data);
+			if (status === 'fetched') setIsLoading(false);
+		},
+		[ status, data ]
+	);
 
 	return (
-		<div>
-			<h1>helooooo</h1>
-			{/* <h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1>
-			<h1>hiiiii</h1> */}
+		<Box>
+			<Heading>helooooo</Heading>
 			<br />
 
-			<Heading>{productId}</Heading>
-		</div>
+			<SimpleGrid columns={3} gap={5}>
+				<GridItem columns={1}>
+					<Skeleton isLoaded={!isLoading}>
+						{product.imgPath ? (
+							<Image src={`${SERVER_URL}${product.imgPath}`} alt="product Image" w="100%" />
+						) : (
+							<Image src="" alt="loading" w="100%" />
+						)}
+					</Skeleton>
+				</GridItem>
+				<GridItem colSpan={2}>
+					<Heading>{productId}</Heading>
+					<Skeleton isLoaded={!isLoading}>
+						<Heading w="100%">{product.name}</Heading>
+						<Text>{product.description}</Text>
+					</Skeleton>
+				</GridItem>
+			</SimpleGrid>
+		</Box>
 	);
 };
