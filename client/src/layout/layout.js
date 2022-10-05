204a731e73765ccdb13124da3c388ede8a3566a5
adi-H <adi.haimsohn@gmail.com>
2022-10-05 16:48:32 +0300

    fixed that weird light line on top of page in light mode

--- client/src/layout/layout.js
+++ client/src/layout/layout.js
@@ -4,21 +4,21 @@ import { NavBar } from '../components/navbar/navbar';
 import { Outlet } from 'react-router-dom';
 import { useColorModeValue } from '@chakra-ui/react';
 import '../assets/stylesheets/main.css';
-// DOCUMENTATION
-// https://stackoverflow.com/a/69982280
+import { darkModeAppBg, lightModeAppBg } from '../theme/variables';
 
-// const appBg = useColorModeValue('#f4f4f2', '#1a202c');
+// * DOCUMENTATION
+// * https://stackoverflow.com/a/69982280
 
 const Layout = ({ children }) => {
-	const appBg = useColorModeValue('#f4f4f2', '#1a202c');
+	const appBg = useColorModeValue(lightModeAppBg, darkModeAppBg);
 
 	return (
-		<VStack className="Layout" align="stretch" spacing={3}>
+		<VStack className="Layout" align="stretch" spacing={3} bg={appBg}>
 			<Flex position="relative" zIndex={8} top={0}>
 				<NavBar />
 			</Flex>
 			<Box pt="70px">
-				<Grid templateColumns="repeat(6, 1fr)" bg={appBg}>
+				<Grid templateColumns="repeat(6, 1fr)">
 					<GridItem colSpan={1} />
 					<GridItem colSpan={4}>
 						<Box className="main-view-container" position="relative">
---
+++ client/src/theme/variables.js
@@ -0,0 +1,3 @@
+export const lightModeAppBg = '#f4f4f2';
+
+export const darkModeAppBg = '#1a202c';
