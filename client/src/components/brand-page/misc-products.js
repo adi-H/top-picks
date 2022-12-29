9431c48e89c07116c21e776a7e6daa6716bcfa30
adi-H <adi.haimsohn@gmail.com>
2022-12-29 11:02:20 +0200

    added generic search comp

--- client/src/components/brand-page/misc-products.js
+++ client/src/components/brand-page/misc-products.js
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem, Heading, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import { Box, Grid, GridItem, Heading, SimpleGrid, Skeleton, Input } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import { useFetch } from '../../hooks/use-fetch';
 import { getProductsByBrand } from '../../services/products';
@@ -7,6 +7,7 @@ import { SmallProductCard } from './small-product-card';
 export const MiscProducts = ({ brandId }) => {
 	let [ isLoading, setIsLoading ] = useState(true);
 	let [ products, setProducts ] = useState([]);
+	let [ search, setSearch ] = useState('');
 	let conf = getProductsByBrand(brandId);
 	let { status, data } = useFetch(conf);
 
@@ -19,24 +20,37 @@ export const MiscProducts = ({ brandId }) => {
 		[ status, data ]
 	);
 
+	const handleSearchTextChange = (event) => setSearch(event.target.value);
+
 	// TODO add a normal search comp?? maybe generic enough so it can be reused ?
 	return (
 		<Box m={4}>
 			<Heading size="sm"> available products by the brand ~~ </Heading> <br />
 			<Grid templateColumns="repeat(5, 1fr)">
-				<GridItem bgColor="red">search comp</GridItem>
+				<GridItem>
+					search comp
+					<Input
+						size="md"
+						variant="filled"
+						placeholder="filter your products"
+						value={search}
+						onChange={handleSearchTextChange}
+					/>
+				</GridItem>
 				<GridItem colSpan={4}>
 					<Skeleton isLoaded={!isLoading}>
 						<SimpleGrid columns={4}>
-							{products.map((p) => (
-								<SmallProductCard
-									key={p.id}
-									id={p.id}
-									name={p.name}
-									avgRating={p.avgRating}
-									imgPath={p.imgPath}
-								/>
-							))}
+							{products
+								.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+								.map((p) => (
+									<SmallProductCard
+										key={p.id}
+										id={p.id}
+										name={p.name}
+										avgRating={p.avgRating}
+										imgPath={p.imgPath}
+									/>
+								))}
 						</SimpleGrid>
 					</Skeleton>
 				</GridItem>
